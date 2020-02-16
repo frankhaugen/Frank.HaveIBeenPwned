ec9505ae6b963192e9d8d0bb24564db73968995d
frank <frank.haugen@gmail.com>
2020-02-16 18:15:20 +0100

    Added token for replacement

--- HIBP.Toolkit.Tests/HibpClientTests.cs
+++ HIBP.Toolkit.Tests/HibpClientTests.cs
@@ -21,7 +21,7 @@ namespace HIBP.Toolkit.Tests
         private const string username = "frank@gmail.com";
         private const string password = "Password";
         private const string website = "adobe";
-        private const string apiKey = "<<apikey>>";
+        private const string apiKey = "#{APIKEY}#";
 
         public HibpClientTests(ITestOutputHelper outputHelper)
         {
